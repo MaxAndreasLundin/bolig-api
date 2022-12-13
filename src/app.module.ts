d983d5baf779dcbe68bbd96d8486c8bf664fa307
MaxAndreasLundin <lundin-89@hotmail.com>
2022-12-13 18:04:37 +0100

    Prisma setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PropertyModule } from './property/property.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, PropertyModule],
+  imports: [AuthModule, UserModule, PropertyModule, PrismaModule],
 })
 export class AppModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
   signup() {
     return { msg: 'I have singed up' };
   }
