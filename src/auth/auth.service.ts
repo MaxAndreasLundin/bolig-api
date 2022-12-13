78634e2a3cbcb95b4371e149336624ecd6a9fa9f
MaxAndreasLundin <lundin-89@hotmail.com>
2022-12-13 21:56:21 +0100

    writing to db works

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup() {
-    return { msg: 'I have singed up' };
+
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+        firstName: dto.firstName,
+        lastName: dto.lastName,
+      },
+    });
+
+    return user;
   }
 
   signin() {
