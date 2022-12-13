181c5fa89b6d6768a0bfeb1741c1727279977b29
MaxAndreasLundin <lundin-89@hotmail.com>
2022-12-13 17:46:13 +0100

    prisma models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { PropertyModule } from './property/property.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, PropertyModule],
 })
 export class AppModule {}
