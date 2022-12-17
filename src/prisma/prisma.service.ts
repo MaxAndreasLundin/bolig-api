1b0053ebcd603d57d1da8ed38703f8e4405372ff
MaxAndreasLundin <lundin-89@hotmail.com>
2022-12-17 18:27:36 +0100

    preparing for testing

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -13,4 +13,11 @@ export class PrismaService extends PrismaClient {
       },
     });
   }
+
+  cleanDb() {
+    return this.$transaction([
+      this.property.deleteMany(),
+      this.user.deleteMany(),
+    ]);
+  }
 }
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,11 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
+import { PrismaService } from '../src/prisma/prisma.service';
 
 describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -17,11 +19,40 @@ describe('App e2e', () => {
       }),
     );
     await app.init();
+
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
   });
 
   afterAll(() => {
     app.close();
   });
 
-  it.todo('should pass');
+  describe('Auth', () => {
+    describe('Signup', () => {
+      it.todo('should signup');
+    });
+
+    describe('Signin', () => {
+      it.todo('should signin');
+    });
+  });
+
+  describe('User', () => {
+    describe('Get me', () => {});
+
+    describe('Edit user', () => {});
+  });
+
+  describe('Properties', () => {
+    describe('Create property', () => {});
+
+    describe('Get property', () => {});
+
+    describe('Get property by id', () => {});
+
+    describe('Edit property', () => {});
+
+    describe('Delete property', () => {});
+  });
 });
