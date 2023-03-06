4681a394519f6b0accf3ec42767f01feb7235a35
MaxAndreasLundin <lundin-89@hotmail.com>
2023-03-06 07:46:18 +0100

    Updated prisma and migrated db.

--- prisma/migrations/20230306064548_updated_entity_values/migration.sql
+++ prisma/migrations/20230306064548_updated_entity_values/migration.sql
@@ -1,16 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Property` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE "Property";
-
--- DropTable
-DROP TABLE "User";
-
 -- CreateTable
 CREATE TABLE "users" (
     "id" SERIAL NOT NULL,
@@ -25,20 +12,26 @@ CREATE TABLE "users" (
 );
 
 -- CreateTable
-CREATE TABLE "properties" (
+CREATE TABLE "estates" (
     "id" SERIAL NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "title" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "link" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "typeOfResidence" TEXT NOT NULL,
+    "coordinates" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "room" INTEGER NOT NULL,
+    "area" INTEGER NOT NULL,
     "userId" INTEGER NOT NULL,
 
-    CONSTRAINT "properties_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "estates_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
 -- AddForeignKey
-ALTER TABLE "properties" ADD CONSTRAINT "properties_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "estates" ADD CONSTRAINT "estates_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
