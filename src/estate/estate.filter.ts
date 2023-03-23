6f8f050232477a1bbd6ae1f504288fd643984b73
MaxAndreasLundin <lundin-89@hotmail.com>
2023-03-23 13:54:57 +0100

    Search estate by category.

---
+++ src/estate/estate.filter.ts
@@ -0,0 +1,19 @@
+export interface EstateFilter {
+  title?: string;
+
+  description?: string;
+
+  link?: string;
+
+  location?: string;
+
+  typeOfResidence?: string;
+
+  coordinates?: string;
+
+  price?: number;
+
+  room?: number;
+
+  area?: number;
+}
