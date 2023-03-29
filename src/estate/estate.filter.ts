ebe2758ae12b53241e76e06c8dc3d2722e06bc78
MaxAndreasLundin <lundin-89@hotmail.com>
2023-03-29 10:13:09 +0200

    Changed to POST and added validation.

--- src/estate/estate.filter.ts
+++ src/estate/estate.filter.ts
@@ -1,30 +1,48 @@
-export interface EstateFilter {
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+
+class Range {
+  @IsOptional()
+  @IsNumber()
+  gte?: number;
+
+  @IsOptional()
+  @IsNumber()
+  lte?: number;
+}
+
+export class EstateFilter {
+  @IsOptional()
+  @IsString()
   title?: string;
 
+  @IsOptional()
+  @IsString()
   description?: string;
 
+  @IsOptional()
+  @IsString()
   link?: string;
 
+  @IsOptional()
+  @IsString()
   location?: string;
 
+  @IsOptional()
+  @IsString()
   typeOfResidence?: string;
 
+  @IsOptional()
+  @IsString()
   coordinates?: string;
 
-  price?: {
-    gte?: number;
-    lte?: number;
-  };
+  @IsOptional()
+  price?: Range;
 
-  room?: {
-    gte?: number;
-    lte?: number;
-  };
+  @IsOptional()
+  room?: Range;
 
-  area?: {
-    gte?: number;
-    lte?: number;
-  };
+  @IsOptional()
+  area?: Range;
 
-  [key: string]: string | number | { gte?: number; lte?: number } | undefined;
+  [key: string]: string | Range | undefined;
 }
