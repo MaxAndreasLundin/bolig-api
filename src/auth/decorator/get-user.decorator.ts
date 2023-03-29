c5f67cefbb2eec04acb79afd013afbfc990233e9
MaxAndreasLundin <lundin-89@hotmail.com>
2023-03-29 21:22:11 +0200

    Removed ts-ignore.

--- src/auth/decorator/get-user.decorator.ts
+++ src/auth/decorator/get-user.decorator.ts
@@ -1,10 +1,10 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { User } from '@prisma/client';
 
 export const GetUser = createParamDecorator(
-  (data: string | undefined, ctx: ExecutionContext) => {
-    const request: Express.Request = ctx.switchToHttp().getRequest();
-    if (data) {
-      // @ts-ignore
+  (data: keyof User | undefined, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest<{ user?: User }>();
+    if (request.user && data) {
       return request.user[data];
     }
     return request.user;
