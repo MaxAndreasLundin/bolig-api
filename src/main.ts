c17d1258f13957dc206c309ab5dbe86942aba5bb
MaxAndreasLundin <lundin-89@hotmail.com>
2023-04-15 10:31:45 +0200

    Fixes for deployment on vercel.

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,39 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+async function createNestApp(expressInstance: express.Express) {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+
+  app.enableCors({
+    origin: ['https://bolig-web.vercel.app', 'http://localhost:3333'],
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
-  await app.listen(parseInt(process.env.PORT ?? '3000'));
+
+  return app.init();
 }
-bootstrap();
+
+const server = express();
+
+createNestApp(server);
+
+if (process.env.VERCEL) {
+  module.exports = (req: VercelRequest, res: VercelResponse) => {
+    server(req, res);
+  };
+} else {
+  server.listen(parseInt(process.env.PORT ?? '3000'), () => {
+    console.log(`Server is running on port ${process.env.PORT ?? '3000'}`);
+  });
+}
